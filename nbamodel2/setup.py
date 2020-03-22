a975394d4da17e94c688714724dea5eb5946e531
RichardP <rcpattison@gmail.com>
2020-03-22 15:15:53 -0700

    updated repo name

--- nbamodel2/setup.py
+++ nbamodel2/setup.py
@@ -1,25 +1,25 @@
 import setuptools
-import apeel_nbamodel2
+import fps_nbamodel2
 
 # Get the version of this package
-version = apeel_nbamodel2.version
+version = fps_nbamodel2.version
 
 # Get the long description of this package
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='apeel_nbamodel2',
+    name='fps_nbamodel2',
     version=version,
-    author="Apeel Data Science",
-    author_email="software@apeelsciences.com",
+    author="RichardP",
+    author_email="rcpattison@gmail.com",
     description="cleaner project folder",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/apeelsciences/datascience/projects/nbamodel2",
+    url="https://github.com/firstprinciples/nbamodel2",
     packages=setuptools.find_packages(exclude=['unit_tests']),
-    install_requires=['apeel_datatools',],
-    package_data={'apeel_nbamodel2': ['models/*.joblib']},
+    install_requires=[],
+    package_data={'fps_nbamodel2': ['models/*.joblib']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
